fec055fc9561afe454cbf6bb29b9f2b0f99d14bd
simcha <simchacop@gmail.com>
2026-07-02 11:07:17 +0300

    feat: valid object with all validations

---
+++ movie-manager/utils/validator.js
@@ -0,0 +1,9 @@
+function createValidator() {
+    return {
+        name: (name) => Boolean(name.trim()),
+        year: (year) => year > 1900 && year <= new Date().getFullYear(),
+        rate: (rate) => rate >= 0 && rate <= 10,
+    };
+}
+
+export const valid = createValidator();
