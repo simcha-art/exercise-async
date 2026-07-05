0e4b0daea6cd398a55dd76f4d1a562f2d167c1d4
simcha <simchacop@gmail.com>
2026-07-05 22:02:15 +0300

    complete all features needed and add all functions to one factory of movieManager and reading from file only once

--- movie-manager/services/movie_service.js
+++ movie-manager/services/movie_service.js
@@ -1,66 +1,74 @@
 import { readFile, writeFile } from "./file_service.js";
 
-const allMovies = await readFile();
 
-function generateId() {
-    let startId;
-    if (allMovies.length === 0) {
-        startId = 1;
-    } else {
-        startId = Math.max(...allMovies.map((movie) => movie.id)) + 1;
+async function createMovieManager () {
+
+    const allMovies = await readFile();
+
+    function generateId() {
+        let startId;
+        if (allMovies.length === 0) {
+            startId = 1;
+        } else {
+            startId = Math.max(...allMovies.map((movie) => movie.id)) + 1;
+        }
+        return () => startId++;
     }
-    return () => startId++;
-}
 
-const getId = generateId();
+    const getId = generateId();
 
-export function showAllMovies() {
-    return allMovies.map((movie) => {
-        return { id: movie.id, title: movie.title };
-    });
-}
+    function showAllMovies() {
+        return allMovies.map((movie) => {
+            return { id: movie.id, title: movie.name };
+        });
+    }
 
-export function showMovieById(id) {
-    return allMovies.find((movie) => movie.id === id);
-}
+    function showMovieById(id) {
+        return allMovies.find((movie) => movie.id === id);
+    }
 
-export function createMovie(name, genre, year, rate) {
-    const id = getId();
-    const newMovie = { id, name, genre, year, rate };
-    allMovies.push(newMovie);
-    writeFile(allMovies);
-    return showMovieById(id);
-}
+    function createMovie(name, genre, year, rate) {
+        const id = getId();
+        const newMovie = { id, name, genre, year, rate };
+        allMovies.push(newMovie);
+        writeFile(allMovies);
+        return showMovieById(id);
+    }
 
-export function deleteMovie(id) {
-    const index = allMovies.findIndex((movie) => movie.id === id);
-    if (index < 0) return;
-    allMovies.splice(index, 1);
-    writeFile(allMovies);
-}
-
-export function updateRate(id, rate) {
-    const movie = allMovies.find((movie) => movie.id === id);
-    if (movie) {
-        movie.rate = rate;
+    function deleteMovie(id) {
+        const index = allMovies.findIndex((movie) => movie.id === id);
+        if (index < 0) return;
+        allMovies.splice(index, 1);
         writeFile(allMovies);
     }
+
+    function updateRate(id, rate) {
+        const movie = allMovies.find((movie) => movie.id === id);
+        if (movie) {
+            movie.rate = rate;
+            writeFile(allMovies);
+        }
+    }
+
+    function searchByName(name) {
+        return allMovies.filter((movie) => movie.name.toLowerCase() === name.toLowerCase());
+    }
+
+    function sortByGenre (genre) {
+        return allMovies.filter((movie) =>  movie.genre.toLowerCase() === genre.toLowerCase())
+    }
+
+    function showStatistics () {
+        const number = allMovies.length
+        let avgRate = allMovies.reduce((acc, current) => acc + current.rate, 0) / number
+        avgRate = Math.round(avgRate * 100) / 100
+        const maxRate = Math.max(...allMovies.map((movie) => movie.rate))
+        return {number, avgRate, maxRate}
+    }
+
+    return {showAllMovies, showMovieById, showStatistics, createMovie, updateRate, deleteMovie, searchByName, sortByGenre}
 }
 
-export function searchByName(name) {
-    return allMovies.filter((movie) => movie.name.toLowerCase() === name.toLowerCase());
-}
+const movieManager =  await createMovieManager()
 
-export function sortByGenre (genre) {
-    return allMovies.filter((movie) =>  movie.genre.toLowerCase() === genre.toLowerCase())
-}
-
-export function showStatistics () {
-    const number = allMovies.length
-    let avgRate = allMovies.reduce((acc, current) => acc + current.rate, 0) / number
-    avgRate = Math.round(avgRate * 100) / 100
-    const maxRate = Math.max(...allMovies.map((movie) => movie.rate))
-    return {number, avgRate, maxRate}
-}
-
-
+export default (movieManager)
